580a5526e9edb9bf1f02bf68845a781fb5e6e049
Noah Paessel <knowuh@gmail.com>
2016-04-13 18:02:40 -0400

    Temporarily remove sticky headers for questions.
    [#117576881] [#115343939]
    
    https://www.pivotaltracker.com/story/show/117576881
    https://www.pivotaltracker.com/story/show/115343939

--- js/components/question-for-class.js
+++ js/components/question-for-class.js
@@ -36,7 +36,10 @@ export default class QuestionForClass extends Component {
 
   renderQuestionHeader() {
     const { question, investigationName, activityName, sectionName, pageName } = this.props
-    const { isSticky } = this.state
+    //const { isSticky } = this.state  TODO: Put this back in when we can make it look like the mockup.
+    // see https://www.pivotaltracker.com/story/show/117576881 & https://www.pivotaltracker.com/story/show/115343939
+    // http://static.concord.org/home/emcelroy/report/index5.html
+    const { isSticky } = false
     if (isSticky) {
       // the sectionName is not used for now
       return <span>{[investigationName, activityName, `Page: ${pageName}`].join(' › ')} › Question #{question.get('questionNumber')}</span>
@@ -46,14 +49,15 @@ export default class QuestionForClass extends Component {
     }
   }
 
-  render() {
+  // TODO: Temporarily removed (see notes in above TODO)
+  renderSticky() {
     const { question } = this.props
     const { answersVisible } = this.state
     return (
       <StickyContainer>
         <div className={`question ${question.get('visible') ? '' : 'hidden'}`}>
           <Sticky className="question-header" onStickyStateChange={this.handleStickyStateChange}>
-            <SelectionCheckbox selected={question.get('selected')} questionKey={question.get('key')} />
+            <SelectionCheckbox selected={question.get('selected')} questionKey={question.get('key')}/>
             { this.renderQuestionHeader() }
             <a className='answers-toggle' onClick={this.toggleAnswersVisibility}>
               {answersVisible ? 'Hide responses' : 'Show responses'}
@@ -64,6 +68,23 @@ export default class QuestionForClass extends Component {
           {answersVisible ? <AnswersTable answers={question.get('answers')}/> : ''}
         </div>
       </StickyContainer>
+    )
+  }
+
+  render() {
+    const { question } = this.props
+    const { answersVisible } = this.state
+    return (
+      <div className={`question ${question.get('visible') ? '' : 'hidden'}`}>
+        { this.renderQuestionHeader() }
+        <a className='answers-toggle' onClick={this.toggleAnswersVisibility}>
+          {answersVisible ? 'Hide responses' : 'Show responses'}
+        </a>
+
+        <QuestionSummary question={question}/>
+        <QuestionDetails question={question}/>
+        {answersVisible ? <AnswersTable answers={question.get('answers')}/> : ''}
+      </div>
     )
   }
 }
