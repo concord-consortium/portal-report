2f79f393a901167ef8b3d6faad87331ec6b5a78c
Noah Paessel <knowuh@gmail.com>
2016-04-15 16:14:12 -0400

    First pass redo of sticky navigation header.
    [#115343939]
    https://www.pivotaltracker.com/story/show/115343939

--- js/components/page.js
+++ js/components/page.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
 import pureRender from 'pure-render-decorator'
 import Question from '../components/question'
-
+import Sticky from 'react-stickynode';
 import '../../css/page.less'
 
-@pureRender
 export default class Page extends Component {
   render() {
     const { page, reportFor, investigationName, activityName, sectionName } = this.props
     const pageName = page.get('name')
     return (
       <div className={`page ${page.get('visible') ? '' : 'hidden'}`}>
-        <h4>Page: {pageName}</h4>
+        <Sticky top={80}>
+          <h4>Page: {pageName}</h4>
+        </Sticky>
         <div>
           {page.get('children').map((question) => {
               return <Question key={question.get('key')} question={question} reportFor={reportFor} investigationName={investigationName} activityName={activityName} sectionName={sectionName} pageName={pageName} />
--- js/components/section.js
+++ js/components/section.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
 import pureRender from 'pure-render-decorator'
 import Page from './page'
-
+import Sticky from 'react-stickynode';
 import '../../css/section.less'
 
-@pureRender
 export default class Section extends Component {
   render() {
     const { section, reportFor, investigationName, activityName } = this.props
     const sectionName = section.get('name')
     return (
       <div className={`section ${section.get('visible') ? '' : 'hidden'}`}>
-        <span className={section.get('nameHidden') ? 'hidden' : ''}>{sectionName}</span>
+        <Sticky top={60} className={section.get('nameHidden') ? 'hidden' : ''}>
+          {sectionName}
+        </Sticky>
         <div>
           {section.get('children').map(p => <Page key={p.get('id')} page={p} reportFor={reportFor} investigationName={investigationName} activityName={activityName} sectionName={sectionName} />)}
         </div>
