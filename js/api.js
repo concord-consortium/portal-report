da3ade719cd2af6a461ad4b51d31fbbbb1894163
Noah Paessel <knowuh@gmail.com>
2018-03-07 13:45:54 -0500

    Chore: Add `standard-js` linting, fix linting errors.

--- js/api.js
+++ js/api.js
@@ -3,19 +3,19 @@ import fakeData from './data/report.json'
 import queryString from 'query-string'
 
 const warn = (message) => {
-  if (console && typeof console.warn == 'function') {
-    console.warn(message);
+  if (console && typeof console.warn === 'function') {
+    console.warn(message)
   }
 }
 const urlParams = (() => {
-  return  queryString.parse(window.location.search)
+  return queryString.parse(window.location.search)
 })()
 
 // Report URL and auth tokens are provided as an URL parameters.
 const REPORT_URL = urlParams['reportUrl']
 const AUTH_HEADER = `Bearer ${urlParams['token']}`
 
-export function fetchReportData() {
+export function fetchReportData () {
   if (REPORT_URL) {
     return fetch(REPORT_URL, {headers: {'Authorization': AUTH_HEADER}})
       .then(checkStatus)
@@ -26,32 +26,31 @@ export function fetchReportData() {
   }
 }
 
-export function updateReportSettings(data) {
+export function updateReportSettings (data) {
   if (REPORT_URL) {
     return fetch(REPORT_URL, {
       method: 'put',
       headers: {
         'Authorization': AUTH_HEADER,
         'Accept': 'application/json',
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify(data)
     })
-  }
-  else {
-    warn("No REPORT_URL. Faking put method.");
+  } else {
+    warn('No REPORT_URL. Faking put method.')
     return new Promise(resolve => {})
   }
 }
 
 export class APIError {
-  constructor(statusText, response) {
+  constructor (statusText, response) {
     this.message = statusText
     this.response = response
   }
 }
 
-function checkStatus(response) {
+function checkStatus (response) {
   if (response.status >= 200 && response.status < 300) {
     return response
   } else {
