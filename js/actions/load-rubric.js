7ef5ab2f854b6a1d92d088ebcab95f5e78004580
Noah Paessel <knowuh@gmail.com>
2018-03-15 10:53:02 -0400

    Feature:  Save & Load rubric (content) using report API.
    We now 'Freeze` rubric after it has been loaded from a URL.  This is to prevent rubric from changing after a teacher has
    graded some or all of their students.
    
    General process:
    
    1. When report data is first loaded from the server, we load rubric for activity feedback, if it exists in serialized form.
    2. After the report data has all been loaded, we check for missing activityFeedback rubric for which we have a URL.
    3. If the URL exists, but we haven't loaded a copy of the rubric yet, we load it.
    4. We update any activities that don't have a serialized version of the rubric JSON using the portal `enableActivityFeedback` api
    
    Main changes:
    1. The `rubricReducer` now handles `RECEIVE_DATA` actions.  It looks for serialized rubric. If it finds some it puts it into the state.
    2. The 'addRubric` action now dispatches `enableActivityFeedback` actions for activities that are missing serialized rubric.
    
    PT Story:
    If a teacher uses a rubric to give feedback, it is copied and locked for them for that assignment, so that it does not change if the author changes it after that point.
    
    [#155368181]
    
    https://www.pivotaltracker.com/story/show/155368181

--- js/actions/load-rubric.js
+++ js/actions/load-rubric.js
@@ -1,17 +1,40 @@
 import fetch from 'isomorphic-fetch'
+import { enableActivityFeedback } from './index'
 
 export const LOAD_RUBRIC = 'LOAD_RUBRIC'
 export const REQUEST_RUBRIC = 'REQUEST_RUBRIC'
+
+const exists = (immutable, fieldName) => {
+  const value = immutable.get(fieldName)
+  return (value && value.length && value.length > 0) ||
+         (value && value.size && value.size > 0)
+}
 
 const urlCache = {}
 const addRubric = (data) => {
   const {url, rubric} = data
   return (dispatch, getState) => {
+    const activities = getState().getIn(['report', 'activities'])
+    const updatable = activities
+      .filter(act => !exists(act, 'rubric'))
+      .filter(act => act.get('rubricUrl') === url)
+
     urlCache[url] = rubric
     dispatch({
       type: LOAD_RUBRIC,
       url,
       rubric
+    })
+
+    updatable.forEach(act => {
+      const activityId = act.get('id')
+      const activityFeedbackId = act.get('activityFeedbackId')
+      const feedbackFlags = {
+        activityFeedbackId,
+        rubric,
+        rubricUrl: url
+      }
+      dispatch(enableActivityFeedback(activityId, feedbackFlags))
     })
   }
 }
@@ -48,16 +71,29 @@ export function loadRubric (data) {
 export function requestRubric () {
   return (dispatch, getState) => {
     const state = getState()
-    const urls = state.getIn(['report', 'activities']).map(a => a.get('rubricUrl'))
-    urls.filter(u => u).forEach((u) => {
+    const activities = state.getIn(['report', 'activities'])
+    const withRubric = activities.filter(act => exists(act, 'rubric') && exists(act, 'rubricUrl'))
+    const withoutRubric = activities.filter(act => !exists(act, 'rubric'))
+    const needingFetch = withoutRubric.filter(act => exists(act, 'rubricUrl'))
+
+    withRubric.forEach(a => {
       dispatch({
-        type: REQUEST_RUBRIC,
-        callAPI: {
-          type: 'loadRubric',
-          data: u,
-          successAction: addRubric
-        }
+        type: LOAD_RUBRIC,
+        url: a.get('rubricUrl'),
+        rubric: a.get('rubric')
       })
     })
+
+    needingFetch.map(a => a.get('rubricUrl'))
+      .forEach((u) => {
+        dispatch({
+          type: REQUEST_RUBRIC,
+          callAPI: {
+            type: 'loadRubric',
+            data: u,
+            successAction: addRubric
+          }
+        })
+      })
   }
 }
--- js/reducers/rubric-reducer.js
+++ js/reducers/rubric-reducer.js
@@ -1,10 +1,18 @@
 import { Map } from 'immutable'
+import { RECEIVE_DATA } from '../actions/index'
 import { LOAD_RUBRIC, REQUEST_RUBRIC } from '../actions/load-rubric'
 
 const INITIAL_RUBRIC_STATE = Map({})
 
 export function rubricReducer (state = INITIAL_RUBRIC_STATE, action) {
   switch (action.type) {
+    case RECEIVE_DATA:
+      const activities = action.response.report.children || []
+      const nextState = activities.reduce((_state, act) => {
+        return _state.set(act.rubric_url, act.rubric)
+      }, state)
+
+      return nextState
     case REQUEST_RUBRIC:
       // NOOP for now.
       return state
