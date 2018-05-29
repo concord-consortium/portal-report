4a3950caeca66807b51ae931c5c42ba18930cad5
Doug Martin <doug@zoopdoop.com>
2018-05-29 16:29:15 -0400

    Add note about why we are parsing the answer for the iframe view work url

--- js/components/iframe-answer.js
+++ js/components/iframe-answer.js
@@ -22,6 +22,18 @@ export default class IframeAnswer extends PureComponent {
   }
 
   getLinkURL(answer) {
+    /*
+    If the author initially sets the interactive to not have a report_url in LARA, then some student works on the interactive,
+    then the author sets the interactive to have a report_url in LARA, the portal will send down the full interactive state to the report.
+    Normally the portal will send down just the report URL as the learner state.
+
+    This problem goes back to LARA. When the interactive is configured as having a report_url, then LARA only sends the report_url
+    to the portal as its state. Otherwise LARA sends all of the interactive state. If the author switches the 'having a report_url'
+    configuration after some students have done work, LARA does update the saved work in the portal.
+
+    A possibly better fix is to have LARA send the updated work to the portal when this is configuration is changed.
+    But it seems wrong to have a little checkbox possibly trigger a massive amount of processing to go on in the background.
+    */
     try {
       const json = JSON.parse(answer)
       const interactiveState = JSON.parse(json.interactiveState)
