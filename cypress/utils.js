ccf688dd42a9f303254a2603a587b2fccc2d810f
Eli Kosminsky <ekosminsky@concord.org>
2018-06-26 18:27:30 -0700

    Updating npm to use npm ci

--- cypress/utils.js
+++ cypress/utils.js
@@ -3,7 +3,7 @@ module.exports = {
   // CSS modules randomize class names, so it is safest to select with explicit data-* attributes
   // https://docs.cypress.io/guides/references/best-practices.html#Selecting-Elements
   getByCypressTag: function ( cypressTag ) {
-    return cy.get('[data-cy=' + cypressTag + ']')
+    return cy.get(`[data-cy=${cypressTag}]`)
   },
 
 };
