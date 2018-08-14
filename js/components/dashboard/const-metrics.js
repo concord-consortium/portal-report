e260df26c270a75ecc888db9504841eaa1e20c9c
Noah Paessel <knowuh@gmail.com>
2018-08-14 10:29:51 -0400

    CSS Changes: Smoother animations using `flex @transition time;` (more)
    * Transition widths first, then heights.
    * Reduce size of collapsed question prompt height.

--- js/components/dashboard/const-metrics.js
+++ js/components/dashboard/const-metrics.js
@@ -1,6 +1,6 @@
 
 export const BOTTOM_MARGIN = 10 // px
 export const COLLAPSED_ACTIVITY_WIDTH = 250 // px
-export const COLLAPSED_ANSWER_WIDTH = 50 // px
+export const COLLAPSED_ANSWER_WIDTH = 60 // px
 export const FULL_ANSWER_WIDTH = 350 // px
 export const MC_SUMMARY_APPROX_WIDTH = 100 // px
