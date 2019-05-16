8b479d9e8157b0f0a2e8daf0dff668d818f53bcd
pjanik <janikpiotrek@gmail.com>
2019-05-16 22:29:06 +0200

    Update most of the dependencies, inc webpack and its config
    [#166004893]

--- js/config.ts
+++ js/config.ts
@@ -1,9 +1,9 @@
 import queryString from "query-string";
 
 const config = (param: string) => {
-  let value = queryString.parse(window.location.search)[param];
+  const value = queryString.parse(window.location.search)[param];
   if (value === "false") {
-    value = false;
+    return false;
   }
   return value;
 };
