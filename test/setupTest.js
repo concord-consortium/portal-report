cbef20939d716272e2b34e1095d5473eec4a7349
pjanik <janikpiotrek@gmail.com>
2019-06-12 14:10:06 +0200

    Fix, update and add new dashboard specs
    [#166414256]

--- test/setupTest.js
+++ test/setupTest.js
@@ -1,13 +1,26 @@
-import { shallow, configure } from "enzyme";
+import { shallow, mount, configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import { Provider } from "react-redux";
+import React from "react";
 
 // see http://airbnb.io/enzyme/docs/installation/index.html
 configure({ adapter: new Adapter() });
 
 // A little helper that lets you quickly pass mock store.
-export const shallowWithStore = (component, store) => {
-  const context = {
-    store
-  };
-  return shallow(component, { context });
+export const shallowWithStore = (component, state) => {
+  const store = { getState: () => state, subscribe: jest.fn(), dispatch: jest.fn(), default: jest.fn() };
+  return shallow(
+    <Provider store={store} >
+      { component }
+    </Provider>
+  );
+};
+
+export const mountWithStore = (component, state) => {
+  const store = { getState: () => state, subscribe: jest.fn(), dispatch: jest.fn(), default: jest.fn() };
+  return mount(
+    <Provider store={store} >
+      { component }
+    </Provider>
+  );
 };
