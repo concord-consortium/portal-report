d8321c44c633890738ec3da0177ca0e849d08919
Scott Cytacki <scytacki@concord.org>
2019-11-08 09:47:46 -0500

    Add cypress code coverage and publish results to code climate
    After this runs, we need to check if the files uploaded to S3 are instrumented or not.
    
    [ #169648963 ]
    https://www.pivotaltracker.com/story/show/169648963

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,7 +14,10 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import "./commands";
+// import "./commands";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+// add code coverage support
+import '@cypress/code-coverage/support'
