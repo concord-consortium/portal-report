1deaa6d8b9007a15745b0ba10eab499071bed491
mklewandowski <matt.kain.lewandowski@gmail.com>
2020-05-19 23:28:52 -0700

    adjust student row CSS/layout

--- js/components/portal-dashboard/student-row.tsx
+++ js/components/portal-dashboard/student-row.tsx
@@ -14,7 +14,9 @@ export class StudentRow extends React.PureComponent<IProps> {
         <div className={css.student}>
           <div className={css.name}>{name}</div>
         </div>
-        <div className={css.responses}>response content</div>
+        <div className={css.responses}>
+          <div>response content</div>
+        </div>
       </div>
     );
   }
