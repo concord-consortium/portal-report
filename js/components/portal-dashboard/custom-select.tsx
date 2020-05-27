550c6d73cbde6598c94cd306d77f60afb368bb50
mklewandowski <matt.kain.lewandowski@gmail.com>
2020-05-27 00:36:38 -0700

    Style updates, hide dialog on click

--- js/components/portal-dashboard/custom-select.tsx
+++ js/components/portal-dashboard/custom-select.tsx
@@ -19,36 +19,66 @@ export interface SelectItem {
 }
 
 export class CustomSelect extends React.PureComponent<IProps, IState> {
+  private divRef = React.createRef<HTMLDivElement>();
+  // private setDivRef: (element: any) => void;
+  private setDivRef = (element: any) => {
+    this.divRef = element;
+  };
+
   constructor(props: IProps) {
     super(props);
     this.state = {
       current: props.items[0].action,
       showList: false
     };
+
+    //this.setDivRef = (element) => {
+    //  this.divRef = element;
+    //};
   }
 
-  render() {
+  public componentDidMount() {
+    document.addEventListener("mousedown", this.handleClick, false);
+  }
+
+  public componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClick, false);
+  }
+
+  public render() {
     const { items } = this.props;
     const currentItem = items.find(i => i.action === this.state.current);
     return (
-      <div className={css.customSelect}>
-        <div className={css.header} onClick={this.handleHeaderClick}>
-          <div className={css.icon}>i</div>
+      <div className={css.customSelect} ref={this.setDivRef}>
+        <div className={css.header + " " + (this.state.showList ? css.showList : "")} onClick={this.handleHeaderClick}>
+          <div className={css.icon}/>
           <div>{currentItem && currentItem.name}</div>
-          <div className={css.icon}>a</div>
+          <div className={css.icon}/>
         </div>
         { this.state.showList &&
           <div className={css.list}>
             { items && items.map((item: SelectItem, i: number) => (
-              <div key={`item ${i}`} className={css.listItem} onClick={this.handleListClick(item.action)}>
-                <div className={css.icon}>c</div>
-                <div className={css.item}>{item.name}</div>
+              <div
+                key={`item ${i}`}
+                className={css.listItem + " " + (this.state.current === item.action ? css.selected : "")}
+                onClick={this.handleListClick(item.action)}
+              >
+                <div className={css.icon}/>
+                <div>{item.name}</div>
               </div>
             )) }
           </div>
         }
       </div>
     );
+  }
+
+  private handleClick = (e: any) => {
+    if (this.divRef.current && e.target && !this.divRef.current.contains(e.target as Node)) {
+      this.setState({
+        showList: false
+      });
+    }
   }
 
   private handleHeaderClick = () => {
