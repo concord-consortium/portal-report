0cda387e40404af707d101f84778e02025eb4a41
eireland <eireland@concord.org>
2020-05-29 07:37:17 -0700

    Removed eslint-plugin-cypress from dependenciesRemoved font specs in sub componentsFixed code formattingChanged data-test attributes to data-cy attributes and fixed tests.Removed progress icon images

--- js/components/portal-dashboard/num-students-container.tsx
+++ js/components/portal-dashboard/num-students-container.tsx
@@ -2,18 +2,18 @@ import React from "react";
 
 import css from "../../../css/portal-dashboard/num-students.less";
 
-  interface IProps {
-    studentCount: number;
-  }
+interface IProps {
+  studentCount: number;
+}
 
 export class NumberOfStudentsContainer extends React.PureComponent<IProps> {
-    render() {
-      return (
-        <div className={css.numStudentsContainer} data-test="num-student-container">
-          <span className={css.containerLabel}>Class: </span>
-          <span data-test="student-number">{this.props.studentCount}</span>
-          <span> students</span>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div className={css.numStudentsContainer} data-cy="num-student-container">
+        <span className={css.containerLabel}>Class: </span>
+        <span data-cy="student-number">{this.props.studentCount}</span>
+        <span> students</span>
+      </div>
+    );
   }
+}
