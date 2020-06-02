39c6224c39340952759a9b0e838785cc82821659
eireland <eireland@concord.org>
2020-06-01 20:38:29 -0700

    WIP header menu Still need to work out the selection of menu items

--- js/components/portal-dashboard/header-menu.tsx
+++ js/components/portal-dashboard/header-menu.tsx
@@ -2,18 +2,44 @@ import React from "react";
 
 import css from "../../../css/portal-dashboard/header.less";
 
-interface IProps {
-  iconId: string;
+interface IState {
+  showMenuItems: boolean;
 }
+export interface MenuItemsNoIcon {
+  name: string;
+}
+export interface MenuItemWithIcon {
+  icon: string;
+  name: string;
+}
+export const itemsNoIcon: MenuItemsNoIcon[] = [
+  { name: "Compact student list" }
+];
+export const items: MenuItemWithIcon[] = [
+  { icon: "#icon-help", name: "Help" },
+  { icon: "#icon-download", name: "Download (.csv)" },
+  { icon: "#icon-print", name: "Print" }
+];
 
-export class HeaderMenuContainer extends React.PureComponent {
+export class HeaderMenuContainer extends React.PureComponent<{}, IState> {
+  constructor() {
+    super({});
+    this.state = {
+      showMenuItems: false
+    };
+    this.handleMenuClick = this.handleMenuClick.bind(this);
+  }
+
   render() {
     return (
       <div className={css.headerMenu} data-cy="header-menu" onClick={this.handleMenuClick}>
-        {this.renderIcon(`${css.icon} ${css.menuIcon}`, "#icon-menu")}
+        {this.renderMenuIcon()}
+        {this.renderMenuItems()}
+        {/* <div className={css.hamBurger}></div> */}
       </div>
     );
   }
+
   private renderIcon = (cssClass: string, iconId: string) => {
     return (
       <svg className={cssClass}>
@@ -22,8 +48,42 @@ export class HeaderMenuContainer extends React.PureComponent {
     );
   }
 
-  private handleMenuClick = () => {
-    // eslint-disable-next-line no-console
-    console.log('menu was clicked');
+  private renderMenuIcon = () => {
+    return (
+      <div>
+        {this.state.showMenuItems ? this.renderIcon(`${css.icon} ${css.menuIcon}`, "#icon-close-menu") : this.renderIcon(`${css.icon} ${css.menuIcon}`, "#icon-menu")}
+      </div>
+    );
   }
+
+  private renderMenuItems = () => {
+    // eslint-disable-next-line no-console
+    console.log("showMenuItems state: ", this.state.showMenuItems);
+    return (
+      <div className={`${css.menuList} ${(this.state.showMenuItems ? css.show : "")}`}>
+        <div className={`${css.topMenu}`}>
+          {itemsNoIcon && itemsNoIcon.map((item: any, i: number) => {
+            return (
+              <div key={`item ${i}`} className={`${css.menuItem}`}>
+                <div className={`${css.menuItemName}`}>{item.name}</div>
+              </div>
+            );
+          })}
+        </div>
+        {items && items.map((item: any, i: number) => {
+          return (
+            <div key={`item ${i}`} className={`${css.menuItem}`}>
+              {this.renderIcon(`${css.menuItemIcon}`, item.icon)}
+              <div className={`${css.menuItemName}`}>{item.name}</div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+
+  private handleMenuClick() {
+    this.setState({ showMenuItems: !this.state.showMenuItems });
+  }
+
 }
