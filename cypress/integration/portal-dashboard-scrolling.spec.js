f6fa525a292a4267a8d317c21faf872cdba45998
mklewandowski <matt.kain.lewandowski@gmail.com>
2020-06-05 18:38:23 -0700

    improve scrolling tests

--- cypress/integration/portal-dashboard-scrolling.spec.js
+++ cypress/integration/portal-dashboard-scrolling.spec.js
@@ -8,9 +8,17 @@ context("Portal Dashboard Content Scrolling",() =>{
     it('verify we scroll and display proper content',()=>{
       cy.viewport(750, 750);
       cy.get('[data-cy=collapsed-activity-button]').first().click();
+      cy.wait(1000);
+
       cy.get('[data-cy=student-answers]').scrollTo('right');
+
       cy.get('[data-cy=collapsed-activity-button]').first().should('be.visible');
       cy.get('[data-cy=collapsed-activity-button]').first().should("contain", "2 Report Test Activity 2");
+
+      cy.get('[data-cy=collapsed-activity-button]').first().click();
+      cy.wait(1000);
+      cy.get('[data-cy=collapsed-activity-button]').first().should('be.visible');
+      cy.get('[data-cy=collapsed-activity-button]').first().should("contain", "1 Report Test Activity 1");
     });
 
 });
