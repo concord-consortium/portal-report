b2a7bbce0d65c3962c85c7b3e90085e0033ded68
eireland <eireland@concord.org>
2020-06-12 10:58:41 -0700

    Added sequence dropdown to header Removed width and height css spec for customSelect so that whoever reuses that component can specify width and height

--- js/components/portal-dashboard/header.tsx
+++ js/components/portal-dashboard/header.tsx
@@ -3,14 +3,17 @@ import React from "react";
 import ccLogoSrc from "../../../img/cc-logo.png";
 import { HeaderMenuContainer } from "./header-menu";
 import { AccountOwnerDiv } from "./account-owner";
+import { CustomSelect } from "./custom-select";
 import css from "../../../css/portal-dashboard/header.less";
 
 interface IProps {
   userName: string;
+  sequenceName: string;
   setCompact: (value: boolean) => void;
+  trackEvent: (category: string, action: string, label: string) => void;
 }
 
-export class Header extends React.PureComponent <IProps> {
+export class Header extends React.PureComponent<IProps> {
   render() {
     return (
       <div className={css.dashboardHeader} data-cy="dashboard-header">
@@ -19,11 +22,32 @@ export class Header extends React.PureComponent <IProps> {
           <div className={css.pin} />
           <div className={css.title}>Dashboard</div>
         </div>
-        <div className={css.assignment}>Assignment:</div>
+        <div className={css.headerCenter}>
+          <div className={css.assignmentTitle}>
+            Assignment:
+        </div>
+          {this.renderAssignmentSelect()}
+        </div>
+
         <div className={css.headerRight}>
-          <AccountOwnerDiv userName={this.props.userName}/>
+          <AccountOwnerDiv userName={this.props.userName} />
           <HeaderMenuContainer setCompact={this.props.setCompact} />
         </div>
+      </div>
+    );
+  }
+
+  private renderAssignmentSelect = () => {
+    const { sequenceName, trackEvent } = this.props;
+    return (
+      <div className={`${css.customSelect}`}>
+        <CustomSelect
+          items={[{ action: "", name: sequenceName }]}
+          onSelectItem={(() => { })}
+          trackEvent={trackEvent}
+          iconId={"icon-assignment"}
+          dataCy={"choose-assignment"}
+        />
       </div>
     );
   }
