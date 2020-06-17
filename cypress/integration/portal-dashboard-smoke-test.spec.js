29c5189841282620828f114154e7e69116452fe8
mklewandowski <matt.kain.lewandowski@gmail.com>
2020-06-17 14:41:56 -0700

    Add tests to anonymize and sort students

--- cypress/integration/portal-dashboard-smoke-test.spec.js
+++ cypress/integration/portal-dashboard-smoke-test.spec.js
@@ -20,13 +20,6 @@ context("Portal Dashboard UI",()=>{
             cy.get('[data-cy=choose-class]').should('be.visible');
             cy.get('[data-cy=sort-students]').should('be.visible');
             cy.get('[data-cy=anonymize-students]').should('be.visible');
-            cy.get('[data-cy=anonymize-students]').within(() => {
-                cy.get('[data-cy=toggle-control]').click();
-            });
-            cy.get('[data-cy=student-name]')
-                .each(function(student, i){
-                    cy.get(student).should("contain", "Student");
-                });
             cy.get('[data-cy=students-feedback]').should('be.visible');
             cy.get('[data-cy=students-feedback]').within(() => {
                 cy.get('[data-cy=toggle-control]').click();
