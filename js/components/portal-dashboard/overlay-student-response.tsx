9f0ebe1ed070254257a71ea1966d373723f679ce
eireland <eireland@concord.org>
2020-06-29 16:05:39 -0700

    added abiity ro roggle to different student names in overlay. Responses are still hard coded

--- js/components/portal-dashboard/overlay-student-response.tsx
+++ js/components/portal-dashboard/overlay-student-response.tsx
@@ -9,29 +9,68 @@ interface IProps {
   students: any;
   isAnonymous: boolean;
 }
+interface IState {
+  currentStudent: any;
+}
 
-export class StudentResponse extends React.PureComponent<IProps> {
+export class StudentResponse extends React.PureComponent<IProps, IState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      currentStudent: this.props.students.first()
+    };
+  }
   render() {
-    const { students, isAnonymous } = this.props;
     return (
       <div data-cy="overlay-student-response-area">
-            <div className={css.responseHeader}>
-              <div className={css.title}>Student Name</div>
-              <div className={css.nextStudentButtons}>
-                <div className={css.button}
-                  data-cy="previous-student-button">
-                  <ArrowLeftIcon className={css.icon} />
-                </div>
-                <div className={css.button}
-                  data-cy="next-student-button">
-                  <ArrowLeftIcon className={css.icon} />
-                </div>
-              </div>
+        <div className={css.responseHeader}>
+          <div className={css.title}>{getFormattedStudentName(this.props.isAnonymous, this.state.currentStudent)}</div>
+          <div className={css.nextStudentButtons}>
+            <div className={css.button + (this.previousStudent ? "" : " " + css.disabled) }
+              data-cy="previous-student-button" onClick={this.toggleStudent(this.previousStudent)}>
+              <ArrowLeftIcon className={css.icon} />
             </div>
+            <div className={css.button + (this.nextStudent ? "" : " " + css.disabled )}
+              data-cy="next-student-button" onClick={this.toggleStudent(this.nextStudent)}>
+              <ArrowLeftIcon className={css.icon} />
+            </div>
+          </div>
+        </div>
         <div className={css.responseArea}>
           This is where student's response will go
         </div>
       </div>
     );
+  }
+
+  private toggleStudent = (student: any) => () => {
+    if (student) {
+      this.setState({ currentStudent: student });
+    }
+    console.log("toggleStudent was clicked: ", student);
+  }
+
+  private get previousStudent() {
+    const { students } = this.props;
+    const studentArr = students.toArray();
+    const student = this.state.currentStudent;
+    if (!students) return false;
+    const idx = studentArr.indexOf(student);
+    if (idx > 0) {
+      return studentArr[idx - 1];
+    }
+    return false;
+  }
+
+  private get nextStudent() {
+    const { students } = this.props;
+    const studentArr = students.toArray();
+    const student = this.state.currentStudent;
+    if (!students) return false;
+    const idx = studentArr.indexOf(student);
+    if (idx < studentArr.length - 1) {
+      return studentArr[idx + 1];
+    }
+    return false;
   }
 }
