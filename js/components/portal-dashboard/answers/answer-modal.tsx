e30c4a913e3c136816afa9141ac51d1782bf7b1a
eireland <eireland@concord.org>
2020-09-08 13:09:47 -0700

    refactored the modal header and body, and css associated with them

--- js/components/portal-dashboard/answers/answer-modal.tsx
+++ js/components/portal-dashboard/answers/answer-modal.tsx
@@ -18,43 +18,59 @@ interface IProps {
 export class AnswerModal extends PureComponent<IProps> {
 
   render() {
-    const { answer, show, onHide, question, studentName } = this.props;
-    const lightboxHeaderClass = `${css.lightboxHeader} ${css.answer}`;
+    const { answer, show, onHide, question } = this.props;
+    const isQuestion = false;
     const type = answer?.get("questionType");
     const questionType = QuestionTypes.find(qt => qt.type === type);
     const QuestionIcon = questionType?.icon;
     const prompt = question && renderHTML(question.get("prompt"));
-    const contentAnswer = answer.get("answer");
-    const answerText = contentAnswer.get("text") || "";
 
     if (!answer) { return null; }
     return (
       <Modal show={show} onHide={onHide} animation={false} dialogClassName={css.lightbox} centered data-cy="answer-lightbox">
-          <Modal.Header className={lightboxHeaderClass} data-cy="modal-header">
-            <div className={css.studentName} data-cy="student-name">
-              {studentName}
-            </div>
-            <div className={css.closeButton} data-cy="close-button" onClick={this.handleCloseLightbox}>
-              <SmallCloseIcon className={css.closeIconSVG} />
-            </div>
-          </Modal.Header>
-          <Modal.Body className={css.lightboxBody}>
-            <div className={css.questionPrompt} data-cy="question-prompt">
-              <QuestionIcon className={`${css.icon} ${css.questionTypeIcon}`} />
-              <span className={css.questionPromptText} data-cy="question-prompt-text">
-                {prompt}
-              </span>
-            </div>
-            <div className={css.studentResponse} data-cy="student-response-area">
-              <div className={css.contentArea} data-cy="response-content">
-                {this.renderAnswer(type)}
-              </div>
-              <div className={css.responseText} data-cy="response-note">
-                {answerText}
-              </div>
-            </div>
-          </Modal.Body>
+        {this.renderModalHeader(isQuestion)}
+        <Modal.Body className={css.lightboxBody}>
+          <div className={css.questionPrompt} data-cy="question-prompt">
+            <QuestionIcon className={`${css.icon} ${css.questionTypeIcon}`} />
+            <span className={css.questionPromptText} data-cy="question-prompt-text">
+              {prompt}
+            </span>
+          </div>
+          <div className={css.contentArea} data-cy="lightbox-content-area">
+            {this.renderModalAnswerContent()}
+          </div>
+        </Modal.Body>
       </Modal>
+    );
+  }
+
+  private renderModalHeader = (isQuestion: boolean) => {
+    const { studentName } = this.props;
+
+    return (
+      <Modal.Header className={css.lightboxHeader + (isQuestion ? css.question : " " + css.answer)} data-cy="modal-header">
+        <div className={css.studentName} data-cy="student-name">
+          {studentName}
+        </div>
+        <div className={css.closeButton} data-cy="close-button" onClick={this.handleCloseLightbox}>
+          <SmallCloseIcon className={css.closeIconSVG} />
+        </div>
+      </Modal.Header>
+    );
+  }
+
+  private renderModalAnswerContent = () => {
+    const { answer } = this.props;
+    const contentAnswer = answer.get("answer");
+    const answerText = contentAnswer.get("text") || "";
+
+    return (
+      <div className={css.studentResponse} data-cy="student-response-area">
+        {this.renderAnswer()}
+        <div className={css.responseText} data-cy="response-note">
+          {answerText}
+        </div>
+      </div>
     );
   }
 
@@ -65,11 +81,11 @@ export class AnswerModal extends PureComponent<IProps> {
     }
   }
 
-  private renderAnswer = (type: string) => {
+  private renderAnswer = () => {
     const { answer } = this.props;
     const contentAnswer = answer.get("answer");
 
-    return(
+    return (
       <img src={contentAnswer.get("imageUrl")} data-cy="answer-image" />
     );
   }
