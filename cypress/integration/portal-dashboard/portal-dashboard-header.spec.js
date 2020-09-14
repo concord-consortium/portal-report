349f8ca78e2b90ab22a0c4e347699bacb6b36f84
mklewandowski <matt.kain.lewandowski@gmail.com>
2020-09-14 15:33:37 -0700

    Fix lint issues

--- cypress/integration/portal-dashboard/portal-dashboard-header.spec.js
+++ cypress/integration/portal-dashboard/portal-dashboard-header.spec.js
@@ -2,7 +2,7 @@ context("Portal Dashboard Header",() =>{
   before(()=>{
       cy.visit("/?portal-dashboard", {
         onBeforeLoad(win) {
-         cy.stub(win, "open");  
+         cy.stub(win, "open");
         }
       });
   });
@@ -13,7 +13,7 @@ context("Portal Dashboard Header",() =>{
         cy.get("[data-cy=header-menu]").click();
         cy.get("[data-cy=help-menu-item").click();
         cy.window().its("open").should("be.called");
-      })
+      });
     });
     describe("Response Area Compact Mode",() =>{
       it("verify we start in non-compact response table mode",()=>{
@@ -24,23 +24,23 @@ context("Portal Dashboard Header",() =>{
       it("verify we enter compact response table mode on menu item click",()=>{
           cy.get("[data-cy=header-menu]").click();
           cy.get("[data-cy=compact-menu-item]").should("be.visible").click();
-  
+
           cy.get("[data-cy=student-answers-row]").should(($el) => {
             expect($el).to.have.css("height", "32px");
           });
       });
     });
-  })
+  });
   context("Header elements are present",()=>{
     describe("Header left", ()=>{
       it("verify logo is visible",()=>{
         cy.get("[data-cy=header-logo").should("be.visible");
       });
-    })
+    });
     describe("Header center",()=>{
       it("verify assignment title is visible",()=>{
         cy.get("[data-cy=choose-assignment]").should("be.visible");
-      })
-    })
-  })
+      });
+    });
+  });
 });
