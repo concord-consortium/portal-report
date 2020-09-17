7cf465c610aba9d866cb25d5ddbb7ae9a84bdbe6
mklewandowski <matt.kain.lewandowski@gmail.com>
2020-09-16 19:58:54 -0700

    Fix handling of scroll state when scrollLeft is 0

--- js/containers/portal-dashboard/portal-dashboard-app.tsx
+++ js/containers/portal-dashboard/portal-dashboard-app.tsx
@@ -204,7 +204,7 @@ class PortalDashboardApp extends React.PureComponent<IProps, IState> {
 
   private handleScroll = (e: React.UIEvent<HTMLElement>) => {
     const element = e.target as HTMLElement;
-    if (element && element.scrollLeft) {
+    if (element && element.scrollLeft !== undefined) {
       this.setState({ scrollLeft: -1 * element.scrollLeft });
     }
   }
