9b520687a4089996f1a606398fb965f83f25c373
eireland <eireland@concord.org>
2020-12-11 14:37:25 -0800

    Refactored StudentNavigation

--- js/components/portal-dashboard/student-navigator.tsx
+++ js/components/portal-dashboard/student-navigator.tsx
@@ -12,15 +12,40 @@ interface IProps {
   setCurrentStudent: (studentId: string | null) => void;
   currentStudentId: string | null;
   inResponseDetail?: boolean;
+  key?: any;
 }
 
 export const StudentNavigator: React.FC<IProps> = (props) => {
+  const { inResponseDetail } = props;
+  const componentOrder = inResponseDetail ? [<PrevNextButtons key={"prev-next-button"} {...props}/>, <StudentName key={"student-name"} {...props}/>]
+                                          : [<StudentName key={"student-name"} {...props}/>, <PrevNextButtons key={"prev-next-button"} {...props}/>];
+  return (
+    <div className={css.studentArea}>
+      <div className={css.responseHeader}>
+        {componentOrder.map(component => component)}
+      </div>
+      {inResponseDetail && <div className={css.studentAreaFiller}></div>}
+    </div>
+  );
+};
+
+const StudentName: React.FC<IProps> = (props) => {
   const { students, isAnonymous, currentStudentIndex, inResponseDetail } = props;
   const studentSelected = currentStudentIndex >= 0;
   const studentName = studentSelected? getFormattedStudentName(isAnonymous, students.get(currentStudentIndex)) : "Student Response";
 
+  return (
+    <div className={`${css.title} ${inResponseDetail ? "" : css.overlay}`} data-cy='overlay-student-name'>
+      {inResponseDetail && <span className={css.studentLabel}>Student: </span>}{studentName}
+    </div>
+  );
+};
+
+const PrevNextButtons: React.FC<IProps> = (props) => {
+  const { students, currentStudentId, currentStudentIndex } = props;
+  const studentSelected = currentStudentIndex >= 0;
+
   const changeCurrentStudent = (index: number) => () => {
-    const { currentStudentId, students } = props;
     const newIndex = Math.min(Math.max(0, index), props.students.size - 1);
     const currentStudentIndex = students.findIndex((s: any) => s.get("id") === currentStudentId);
     if (currentStudentIndex !== newIndex) {
@@ -29,38 +54,16 @@ export const StudentNavigator: React.FC<IProps> = (props) => {
     }
   };
 
-  const StudentName: React.FC = () => {
-    return (
-      <div className={`${css.title} ${inResponseDetail ? "" : css.overlay}`} data-cy='overlay-student-name'>
-        {inResponseDetail && <span className={css.studentLabel}>Student: </span>}{studentName}
-      </div>
-    );
-  };
-
-  const PrevNextButtons: React.FC = () => {
-    return (
-      <div className={css.nextStudentButtons}>
-      <div className={`${css.button} ${(studentSelected && currentStudentIndex > 0) ? "" : css.disabled}`}
-        data-cy="previous-student-button" onClick={changeCurrentStudent(currentStudentIndex - 1)}>
-        <ArrowLeftIcon className={css.icon} />
-      </div>
-      <div className={`${css.button} ${(studentSelected && currentStudentIndex < students.size - 1) ? "" : css.disabled}`}
-        data-cy="next-student-button" onClick={changeCurrentStudent(currentStudentIndex + 1)}>
-        <ArrowLeftIcon className={css.icon} />
-      </div>
-    </div>
-    );
-  };
-
-  const componentOrder = inResponseDetail ? [<PrevNextButtons key={1}/>, <StudentName key={studentName}/>]
-                                             : [<StudentName key={studentName}/>, <PrevNextButtons key={2}/>];
-
   return (
-    <div className={css.studentArea}>
-      <div className={css.responseHeader}>
-        {componentOrder.map(component => component)}
-      </div>
-      {inResponseDetail && <div className={css.studentAreaFiller}></div>}
+    <div className={css.nextStudentButtons}>
+    <div className={`${css.button} ${(studentSelected && currentStudentIndex > 0) ? "" : css.disabled}`}
+      data-cy="previous-student-button" onClick={changeCurrentStudent(currentStudentIndex - 1)}>
+      <ArrowLeftIcon className={css.icon} />
     </div>
+    <div className={`${css.button} ${(studentSelected && currentStudentIndex < students.size - 1) ? "" : css.disabled}`}
+      data-cy="next-student-button" onClick={changeCurrentStudent(currentStudentIndex + 1)}>
+      <ArrowLeftIcon className={css.icon} />
+    </div>
+  </div>
   );
 };
