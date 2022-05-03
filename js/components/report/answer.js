b7ff8465ab63f8a0d25917eb5cfb8b77eeaa5b1f
Unknown <emcelroy@concord.org>
2022-05-03 15:53:09 -0400

    Add open response answerText check to  the report Answer component, too.

--- js/components/report/answer.js
+++ js/components/report/answer.js
@@ -1,19 +1,13 @@
 import React, { PureComponent } from "react";
 import MultipleChoiceAnswer from "./multiple-choice-answer";
+import OpenResponseAnswer from "./open-response-answer";
 import ImageAnswer from "./image-answer";
 import IframeAnswer from "./iframe-answer";
 import NoAnswer from "./no-answer";
 import { renderInvalidAnswer } from "../../util/answer-utils";
 
-const AnswerComponent = {
-  "open_response": IframeAnswer,
-  "multiple_choice": MultipleChoiceAnswer,
-  "image_question": ImageAnswer,
-  "iframe_interactive": IframeAnswer,
-  "NoAnswer": NoAnswer,
-};
-
 export default class Answer extends PureComponent {
+
   render() {
     const { question, answer, alwaysOpen, answerOrientation, reportItemAnswer } = this.props;
     if (typeof answer === "undefined") {
@@ -21,6 +15,15 @@ export default class Answer extends PureComponent {
       // it seems its not. TBD later.
       return <div data-cy="no-response">No response</div>;
     }
+    const AnswerComponent = {
+      // Answers to LARA-native open response questions must be handled differently than
+      // answers to managed interactive open response questions.
+      "open_response": answer.has("answerText") ? IframeAnswer : OpenResponseAnswer,
+      "multiple_choice": MultipleChoiceAnswer,
+      "image_question": ImageAnswer,
+      "iframe_interactive": IframeAnswer,
+      "NoAnswer": NoAnswer,
+    };
     const AComponent = AnswerComponent[answer.get("questionType")];
     if (!AComponent) {
       return renderInvalidAnswer("Answer type not supported");
