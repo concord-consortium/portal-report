81004cd373d07153eae1f535a1cdab09f6d2f4fd
Ethan McElroy <emcelroy@concord.org>
2025-05-19 18:24:53 -0400

    feat: style update to activity buttons (CLASSDASH-71)
    [CLASSDASH-71](https://concord-consortium.atlassian.net/browse/CLASSDASH-71)

--- cypress/integration/portal-dashboard/portal-dashboard-scrolling.spec.js
+++ cypress/integration/portal-dashboard/portal-dashboard-scrolling.spec.js
@@ -9,13 +9,13 @@ context("Portal Dashboard Content Scrolling",() =>{
     cy.get('[data-cy=progress-table]').scrollTo('right');
 
     cy.get('[data-cy=collapsed-activity-button]').first().should('be.visible');
-    cy.get('[data-cy=collapsed-activity-button]').first().should("contain", "2 Report Test");
+    cy.get('[data-cy=collapsed-activity-button]').first().should("contain", "2: Report Test");
 
     cy.get('[data-cy=collapsed-activity-button]').first().click();
     cy.wait(1000);
     cy.get('[data-cy=progress-table]').scrollTo('left');
     cy.get('[data-cy=collapsed-activity-button]').first().should('be.visible');
-    cy.get('[data-cy=collapsed-activity-button]').first().should("contain", "1 Report Test");
+    cy.get('[data-cy=collapsed-activity-button]').first().should("contain", "1: Report Test");
   });
 
 });
